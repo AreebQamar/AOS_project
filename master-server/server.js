4a3fa21b4e3c70214b62ba8bf82df7b6052e8734
Areeb Qamar <areeb.qamar1225@gmail.com>
2024-05-23 18:45:31 +0500

    problem with server resolver
    server was not running. now it runs

--- master-server/server.js
+++ master-server/server.js
@@ -2,7 +2,7 @@ const grpc = require("@grpc/grpc-js");
 const protoLoader = require("@grpc/proto-loader");
 const packageDefinition = protoLoader.loadSync("../proto.proto", {});
 const grpcObject = grpc.loadPackageDefinition(packageDefinition);
-const pingpongProto = grpcObject.PingPongPackage;
+const ourFileSystem = grpcObject.distributedFileSyatemPackage;
 
 function ping(call, callback) {
   const clientId = call.request.id;
@@ -10,10 +10,29 @@ function ping(call, callback) {
   callback(null, { message: `Pong from server to client ${clientId}` });
 }
 
+function pingAll(call) {
+  call.on('data', (clientIdentity) => {
+    const clientId = clientIdentity.id;
+    console.log(`Received PingAll from client: ${clientId}`);
+    call.write({ message: `Pong to client ${clientId}` });
+  });
+
+  call.on('end', () => {
+    console.log('PingAll stream ended');
+    call.end();
+  });
+}
+
 function main() {
+
   const server = new grpc.Server();
-  server.addService(pingpongProto.PingPong.service, { Ping: ping });
+  server.addService(ourFileSystem.fileSystem.service, { 
+    Ping: ping,
+    PingAll: pingAll
+  });
+
   const serverAddress = "localhost:50051";
+  
   server.bindAsync(
     serverAddress,
     grpc.ServerCredentials.createInsecure(),
--- storage-server/storage.js
+++ storage-server/storage.js
@@ -2,7 +2,7 @@ const grpc = require("@grpc/grpc-js");
 const protoLoader = require("@grpc/proto-loader");
 const packageDefinition = protoLoader.loadSync("../proto.proto", {});
 const grpcObject = grpc.loadPackageDefinition(packageDefinition);
-const pingpongProto = grpcObject.PingPongPackage;
+const ourFileSystem = grpcObject.distributedFileSyatemPackage;
 
 // Read client ID from command-line arguments
 const args = process.argv.slice(2);
@@ -12,7 +12,7 @@ if (args.length < 1) {
 }
 const clientId = args[0];
 
-const client = new pingpongProto.PingPong(
+const client = new ourFileSystem.fileSystem(
   "localhost:50051",
   grpc.credentials.createInsecure()
 );
@@ -26,5 +26,28 @@ function pingClient() {
     }
   });
 }
+
+function startPingAll() {
+  const call = client.PingAll();
+  
+  // Send client identity
+  call.write({ id: clientId });
+
+  call.on('data', (response) => {
+    console.log('Received from server:', response.message);
+  });
+
+  call.on('end', () => {
+    console.log('PingAll stream ended by server');
+  });
+
+  // Send periodic pings to server
+  setInterval(() => {
+    call.write({ id: clientId });
+  }, 5000);
+}
+
+
+startPingAll();
 
 setInterval(pingClient, 5000);
